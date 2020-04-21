dbbcb256622976b17b21c2237c55adfe98970b70
Sean Dever <seand0611@gmail.com>
2020-04-21 04:16:25 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -5,7 +5,7 @@ def find_item_by_name_in_collection(name, collection)
   result = {}
   collection.each_with_index do |pair, index|
     if name == pair[:item]
-      pair
+      return pair
     end 
   end
   nil
