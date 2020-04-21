079ef4c6c681113fd13f3407526e4911ceafd7ee
Sean Dever <seand0611@gmail.com>
2020-04-21 03:55:58 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,9 +3,7 @@ require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
   collection.each do |pair|
-    if name == collection[:item]
-      collection[:item]
-    end
+    binding.pry
   end
 end
 
