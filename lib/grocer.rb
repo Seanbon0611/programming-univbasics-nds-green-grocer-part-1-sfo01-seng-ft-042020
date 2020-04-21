2c8a709b7a5465fa113bceff592d953ec116f586
Sean Dever <seand0611@gmail.com>
2020-04-21 04:29:16 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -13,7 +13,8 @@ end
 def consolidate_cart(cart)
   result = []
   cart.each do |pair|
-  item = find_item_by_name_in_collection(key, result)
+  item = 
+  item_name = find_item_by_name_in_collection(item, result)
   binding.pry
   end 
 end
